da1eb890cbe001470771910317a5da060128532a
Daniel Balchev <dbalchev@gmail.com>
2024-08-24 19:51:01 +0000

    Don't process the input twice

--- app/Day06.hs
+++ app/Day06.hs
@@ -3,14 +3,15 @@
 
 module Day06 where
 import           AocPrelude
-import           Data.List  (nub, sort)
-import           Prelude    ()
+import           Data.Foldable (Foldable (toList))
+import           Data.List     (nub, sort)
+import           Prelude       ()
 
 solution inputText = (part1, part2)
     where
-        part1 = sum $ map (length . nub . sort . concatMap unpack) answersPerGroup
-        part2 = sum $ map (length . foldl1 intersection . map (fromList @(HashSet Char) . unpack)) answersPerGroup
-        answersPerGroup = map (splitOn "\n") . splitOn "\n\n" $ inputText
+        part1 = sum $ map (length . nub . sort . concatMap unpack) $ toList answersPerGroup
+        part2 = sum $ map (length . foldl1 intersection . map (fromList @(HashSet Char) . unpack) . toList) $ toList answersPerGroup
+        answersPerGroup = fromList @(Vector _) . map (fromList @(Vector _ ) . splitOn "\n") . splitOn "\n\n" $ inputText
 
 -- >>> runSolution solution (TestInput "06")
 -- (11,6)
