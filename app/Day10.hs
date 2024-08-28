6ad7672d9e90666eb2f89e3679b774bf802f09eb
Daniel Balchev <dbalchev@gmail.com>
2024-08-28 16:27:11 +0000

    Day 10 part 2

--- app/Day10.hs
+++ app/Day10.hs
@@ -12,18 +12,27 @@ diffDistribution joltages = foldl foldFn empty (zipWith (-) joltageRestList jolt
         joltageList@(_:joltageRestList) = toList joltages
         foldFn distributionMap delta = alter (Just . maybe 1 (+1)) delta distributionMap
 
-solution input = diffs ! 1 * diffs ! 3
+dp2 :: Vector Int -> Integer
+dp2 joltages = memo ! (length joltages - 1)
+    where
+        go :: Int -> Integer
+        go 0 = 1
+        go n = sum . map (memo !) . takeWhile (\i -> (joltages ! i) >= threshold) $ [(n-1), (n-2)..0]
+            where
+                threshold = joltages ! n - 3
+        memo = fromList @(Vector Integer) . map go $ [0..(length joltages - 1)]
+
+solution input = (diffs ! 1 * diffs ! 3, dp2 joltages)
     where
         inputJoltages = fromList @(Vector Int) . (0:) . sort . map fst . rights . map decimal . words $ input
         joltages = snoc inputJoltages (3 + maximum inputJoltages)
         diffs = diffDistribution joltages
 
-
 -- >>> runSolution solution (TestInput "10.1")
--- 35
+-- (35,8)
 
 -- >>> runSolution solution (TestInput "10.2")
--- 220
+-- (220,19208)
 
 -- >>> runSolution solution (RealInput "10")
--- 1876
+-- (1876,14173478093824)
