3a1752a838ead7f2bf0c0ae8179be9bfc3d69311
Daniel Balchev <dbalchev@gmail.com>
2024-08-30 21:44:32 +0000

    Day12 part 1

---
+++ app/Day12.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE TypeApplications #-}
+module Day12 where
+import           AocPrelude
+import           Prelude    ()
+parseLine line = (instructionType, arg)
+    where
+        Just (instructionType, argStr) = uncons line
+        Right (arg, _) = decimal argStr
+
+updateState (position@(posN, posE), direction@(dirN, dirE)) (instructionType, arg)
+    | instructionType == 'N' = ((posN + arg, posE), direction)
+    | instructionType == 'S' = ((posN - arg, posE), direction)
+    | instructionType == 'E' = ((posN, posE + arg), direction)
+    | instructionType == 'W' = ((posN, posE - arg), direction)
+    | instructionType == 'F' = ((posN + dirN * arg, posE + dirE * arg), direction)
+    | instructionType `member` fromList @(HashSet _) ['L', 'R'] && arg == 180 = (position, (-dirN, -dirE))
+    | (instructionType, arg) `member` fromList @(HashSet _) [('L', 90), ('R', 270)]  = (position, (dirE, -dirN))
+    | (instructionType, arg) `member` fromList @(HashSet _) [('L', 270), ('R', 90)]  = (position, (-dirE, dirN))
+
+solution input = abs lastPosN + abs lastPosE
+    where
+        instructions = fromList @(Vector _) . map parseLine . lines $ input
+        ((lastPosN, lastPosE), _) = foldl updateState ((0, 0), (0, 1)) instructions
+
+
+-- >>> runSolution solution (TestInput "12")
+-- 25
+
+-- >>> runSolution solution (RealInput "12")
+-- 858
