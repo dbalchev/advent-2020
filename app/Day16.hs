bf725017e281c8951b96982feed3600d02f73647
Daniel Balchev <dbalchev@gmail.com>
2024-09-04 09:21:54 +0000

    Day 16 part 2

--- app/Day16.hs
+++ app/Day16.hs
@@ -3,9 +3,11 @@
 {-# LANGUAGE TypeApplications  #-}
 module Day16 where
 import           AocPrelude
-import           Data.Either   (rights)
-import           Data.Foldable (toList)
-import           Prelude       ()
+import           Control.Monad  (guard)
+import           Data.Bifunctor (Bifunctor (second))
+import           Data.Either    (rights)
+import           Data.Foldable  (toList)
+import           Prelude        ()
 
 data ParsedInput = ParsedInput {
     validations    :: HashMap Text (Vector (Int, Int)),
@@ -35,24 +37,59 @@ parseInput input = ParsedInput {validations, myTicketFields, nearbyTickets}
         Just nearbyTicketsStrNoPrefix = stripPrefix "nearby tickets:\n" nearbyTicketsStr
         readTickets = fromList . map fst . rights . map decimal . splitOn ","
         myTicketFields = readTickets myTicketStrNoPrefix
-        nearbyTickets = fromList . map readTickets . lines $ nearbyTicketsStr
+        nearbyTickets = fromList . map readTickets . lines $ nearbyTicketsStrNoPrefix
+
+
+validateField ranges n = any (`valdiateRange` n) ranges
+valdiateRange (low, high) n = low <= n && n <= high
 
 isValidForAny :: HashMap a (Vector (Int, Int)) -> Int -> Bool
 isValidForAny validations number = any (`validateField` number) . elems $ validations
-    where
-        validateField ranges n = any (`valdiateRange` n) ranges
-        valdiateRange (low, high) n = low <= n && n <= high
 
-solution input = solution1
+computePossibleIndices :: Vector (Int, Int) -> Vector (Vector Int) -> HashSet Int
+computePossibleIndices constraints nearbyTickets = fromList $ do
+    index <- [0..(length (nearbyTickets ! 0) - 1)]
+    guard $ all (validateField constraints . (! index)) nearbyTickets
+    return index
+
+trivailSolvePossibleIndices :: HashMap Text (HashSet Int ) -> HashMap Text (HashSet Int)
+trivailSolvePossibleIndices = go
+    where
+        hasSingleElementMapping = any ((== 1) . length)
+        go mapping
+            | not $ hasSingleElementMapping mapping = mapping
+            | otherwise = insert (singleElementKey, fromList [singleElementValue]) $ go cleanedMap
+            where
+                (singleElementKey, singleElementValueSet):_ = filter ((==1) . length . snd) $ toKeyValuePairs mapping
+                [singleElementValue] = toList singleElementValueSet
+                cleanedMap = fromList . map (second (delete singleElementValue)) . filter ((/= singleElementKey) . fst) . toKeyValuePairs $ mapping
+
+extractFields fieldFilter mapping ticket = do
+    (field, indices) <- toKeyValuePairs mapping
+    guard $ fieldFilter field
+    index <- toList indices
+    return $ ticket ! index
+
+
+solution fieldFilter input = (solution1, solution2)
     where
         ParsedInput {validations, myTicketFields, nearbyTickets} = parseInput input
         allNearbyTickets = concatMap toList . toList $ nearbyTickets
         solution1 = sum . filter (not . isValidForAny validations) $ allNearbyTickets
+        validNearbyTickets = fromList @(Vector _ ) . filter (all (isValidForAny validations)) . toList $ nearbyTickets
+        validMappings = fromList @(HashMap _ _). map (\(k, v) -> (k, computePossibleIndices v validNearbyTickets)) $ toKeyValuePairs validations
+        solvedMappings = trivailSolvePossibleIndices validMappings
+        solution2 = product $ extractFields fieldFilter solvedMappings myTicketFields
 
 
--- >>> runSolution solution (TestInput "16")
--- 71
 
--- >>> runSolution solution (RealInput "16")
--- 20058
+-- >>> runSolution (solution (const False)) (TestInput "16")
+-- (71,[])
+
+
+-- >>> runSolution (solution (== "class")) (TestInput "16.2")
+-- (0,12)
+
+-- >>> runSolution (solution ("departure" `isPrefixOf`)) (RealInput "16")
+-- (20058,366871907221)
 
